8cbcc9ecfabdbd5af3871302c1f65dfc2e0fdfaf
Kirill Yurkov <vk1r1ll@yandex.ru>
2020-01-30 22:39:44 +0300

    Updates

---
+++ precache-manifest.27ca2114f170ea8480f1ddc488d81e9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ea37e19c6b9439017847d3bd561199",
+    "url": "./index.html"
+  },
+  {
+    "revision": "219d473f2de1ef4212ed",
+    "url": "./static/css/2.b27b1b61.chunk.css"
+  },
+  {
+    "revision": "d150ee246d5556ec3f19",
+    "url": "./static/css/main.439b4a20.chunk.css"
+  },
+  {
+    "revision": "219d473f2de1ef4212ed",
+    "url": "./static/js/2.fabc9287.chunk.js"
+  },
+  {
+    "revision": "d150ee246d5556ec3f19",
+    "url": "./static/js/main.221caf85.chunk.js"
+  },
+  {
+    "revision": "76a407d02933300ba978",
+    "url": "./static/js/runtime-main.a8558982.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
